2651b34542f8e4472254a1658a902695e3b9b1d8
ethanr2 <ethanrhmn@gmail.com>
2020-12-16 15:45:47 -0600

    Started working on alphavantage script. This may have historical data.

--- CME_DataNoAPI.py
+++ CME_DataNoAPI.py
@@ -13,45 +13,40 @@ import pandas as pd
 
 URL = "https://www.cmegroup.com/CmeWS/mvc/Quotes/Future/305/G?quoteCodes=null&_=1null"
 
-def get_futures(path = ''):
-    df = {
-          'ExpirationMonth': [],
-          'Last': []
-          }
+
+def get_futures(path=""):
+    df = {"ExpirationMonth": [], "Last": []}
     now = dt.now()
-    quotes = requests.get(URL).json()['quotes']
+    quotes = requests.get(URL).json()["quotes"]
     for quote in quotes:
-        if not quote['last'] == '-':
-            print(quote['expirationDate'])
-            stamp = str(quote['expirationDate'])
-            stamp = dt.strptime(stamp, '%Y%m%d')
-            df['ExpirationMonth'].append(stamp)
-            df['Last'].append(float(quote['last']))
-        
+        if not quote["last"] == "-":
+            print(quote["expirationDate"])
+            stamp = str(quote["expirationDate"])
+            stamp = dt.strptime(stamp, "%Y%m%d")
+            df["ExpirationMonth"].append(stamp)
+            df["Last"].append(float(quote["last"]))
+
     df = pd.DataFrame(df)
-    df['Last'] = 1 - df['Last']/100 
-    df['Timestamp'] = now
-    df = df.loc[:,['Timestamp', 'ExpirationMonth', 'Last']]
-    
-    print('CME FFR Futures:')
+    df["Last"] = 1 - df["Last"] / 100
+    df["Timestamp"] = now
+    df = df.loc[:, ["Timestamp", "ExpirationMonth", "Last"]]
+
+    print("CME FFR Futures:")
     print(df)
-    
-    df.to_csv(path + 'data/cme_ffr.csv', mode='a', header=False, index = False)
-    
+
+    df.to_csv(path + "data/cme_ffr.csv", mode="a", header=False, index=False)
+
     return df
+
 
 df = get_futures()
 
 #%%
 
 
-if __name__ == '__main__':
-    data_path = __file__[:__file__.find('CME')]
+if __name__ == "__main__":
+    data_path = __file__[: __file__.find("CME")]
     get_futures(data_path)
 
 
 #%%
-
-
-
-
---
+++ alphavantage.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 16 15:44:24 2020
+
+@author: ethan
+"""
+
