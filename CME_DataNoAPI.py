614fa8d0424aa23e7fb1c8b8e6f508aeeaa9f335
ethanr2 <ethanrhmn@gmail.com>
2020-12-21 20:25:45 -0600

    Updated CME script to scrape LIBOR

--- CME_DataNoAPI.py
+++ CME_DataNoAPI.py
@@ -11,16 +11,29 @@ from datetime import timedelta as td
 import numpy as np
 import pandas as pd
 
-URL = "https://www.cmegroup.com/CmeWS/mvc/Quotes/Future/305/G?quoteCodes=null&_=1null"
+URL_FFR = "https://www.cmegroup.com/CmeWS/mvc/Quotes/Future/305/G?quoteCodes=null&_=1null"
+URL_LIBOR = "https://www.cmegroup.com/CmeWS/mvc/Quotes/Future/1/G?quoteCodes=null&_=1null"
 
+TITLE_FFR = 'CME FFR Futures'
+TITLE_LIBOR = 'CME Eurodollar (LIBOR) Futures'
 
-def get_futures(path=""):
+DATABASE_FFR = 'data/cme_ffr.csv'
+DATABASE_LIBOR = 'data/cme_libor.csv'
+
+ASSETS = pd.DataFrame({
+    'ffr':[URL_FFR, TITLE_FFR, DATABASE_FFR],
+    'libor': [URL_LIBOR, TITLE_LIBOR, DATABASE_LIBOR]
+    }, index = ['url', 'title', 'database'])
+
+def get_futures(asset_key, path="" ):
+    col = ASSETS[asset_key]
+    url = col['url']
+    
     df = {"ExpirationMonth": [], "Last": []}
     now = dt.now()
-    quotes = requests.get(URL).json()["quotes"]
+    quotes = requests.get(url).json()["quotes"]
     for quote in quotes:
         if not quote["last"] == "-":
-            print(quote["expirationDate"])
             stamp = str(quote["expirationDate"])
             stamp = dt.strptime(stamp, "%Y%m%d")
             df["ExpirationMonth"].append(stamp)
@@ -31,22 +44,18 @@ def get_futures(path=""):
     df["Timestamp"] = now
     df = df.loc[:, ["Timestamp", "ExpirationMonth", "Last"]]
 
-    print("CME FFR Futures:")
+    print("{}:".format(col['title']))
     print(df)
 
-    df.to_csv(path + "data/cme_ffr.csv", mode="a", header=False, index=False)
+    df.to_csv(path + col['database'], mode="a", header=False, index=False)
 
     return df
 
-
-df = get_futures()
-
 #%%
-
-
 if __name__ == "__main__":
     data_path = __file__[: __file__.find("CME")]
-    get_futures(data_path)
+    get_futures('ffr',data_path)
+    get_futures('libor',data_path)
 
 
 #%%
