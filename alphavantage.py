2286839662f79ca85ecc8e599322508631a6afd6
ethanr2 <ethanrhmn@gmail.com>
2020-12-31 15:29:55 -0600

    commented out line 55

--- alphavantage.py
+++ alphavantage.py
@@ -5,3 +5,7 @@ Created on Wed Dec 16 15:44:24 2020
 @author: ethan
 """
 
+import 
+
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={}&interval=1min&apikey={}&outputsize=1&datatype=csv'
+
