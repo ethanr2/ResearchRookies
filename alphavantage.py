42d564b70be485933b72855326a4aa97eba05521
ethanr2 <ethanrhmn@gmail.com>
2021-01-14 15:46:02 -0600

    Did some preliminary exploratory data analysis on Alphavantage data

--- alphavantage.py
+++ alphavantage.py
@@ -4,8 +4,146 @@ Created on Wed Dec 16 15:44:24 2020
 
 @author: ethan
 """
+from datetime import date
+from datetime import datetime as dt
+from datetime import timedelta as td
 
-import 
+import numpy as np
+import pandas as pd
+from scipy import stats
 
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={}&interval=1min&apikey={}&outputsize=1&datatype=csv'
+from bokeh.io import export_png,output_file,show
+from bokeh.plotting import figure
+from bokeh.models import NumeralTickFormatter, LabelSet,ColumnDataSource
+from bokeh.models.tickers import FixedTicker
+from bokeh.layouts import row, column
 
+URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol={}&interval=1min&slice=year2month6&apikey={}'
+with open('apiKey.txt') as file:
+    KEY = file.read().split(',')[1]
+
+tks = ['VGSH', 'BSV', 'VGIT', 'VGLT']
+#tks =['US10YT']
+sers = {}
+for t in tks:
+    query = pd.read_csv(URL.format(t,KEY))
+    query['time'] = pd.DatetimeIndex(query['time'])
+    query = query.set_index('time')
+    sers[t]= query['close']
+    print(t)
+    print(query['close'])
+data = pd.DataFrame(sers)
+data
+#%%
+data.to_csv('data/alpha_vantage_frame2.csv')
+#%%
+mtg_date = dt(year=2019,month=7,day = 31, hour = 14)
+df = data.loc[data.index < mtg_date +td(hours = 1),:]
+df = df.loc[df.index > mtg_date -td(minutes = 15),:]
+
+for t in tks:
+    df[t] = df[t].fillna(method = 'ffill')
+    base = df.loc[mtg_date, t]
+    df[t] = df[t]/base
+
+df
+#%%
+
+from bokeh.palettes import plasma
+
+def set_up(x, y, truncated = True, margins = None):
+    if truncated: 
+        b = (3 * y.min() - y.max())/2
+    else:
+        b = y.min()
+    if margins == None:    
+        xrng = (x.min(),x.max())
+        yrng = (b,y.max())
+    else:
+        xrng = (x.min() - margins,x.max() + margins)
+        yrng = (b - margins,y.max() + margins)
+        
+    x = x.dropna()
+    y = y.dropna()
+    
+    return(x,y,xrng,yrng)
+
+# Chart of non-stationary time series, e.g. NGDP from 2008 to 2020    
+def chart0(df):
+    xdata, ydata, xrng, yrng = set_up(df.index,df['___'])
+    
+    p = figure(width = 1000, height = 500,
+               title= '____', 
+               x_axis_label = 'Date', x_axis_type = 'datetime',
+               y_axis_label = '', 
+               y_range = yrng, x_range = xrng)
+    p.line(xrng,[0,0], color = 'black')
+    
+    p.line(xdata,ydata, color = 'blue', legend = '')
+    
+    p.xaxis[0].ticker.desired_num_ticks = 10
+    p.legend.location = 'top_left'
+    p.ygrid.grid_line_color = None
+    p.yaxis.formatter=NumeralTickFormatter(format="____")
+    
+    export_png(p,filename ='imgs/chart0.png')
+
+    return p
+
+# Chart of approximately stionary time series, e.g. PCE-Core inflation from 2008 to 2020
+def chart1(df):
+    xdata, ydata, xrng, yrng = set_up(df.index, df.stack(), truncated = False)
+    marg = .005
+    yrng = (yrng[0]-marg, yrng[1] + marg)
+    
+    p = figure(width = 1000, height = 500,
+               title="Monetary Shock: March 3rd, 2020" , 
+               x_axis_label = 'Time of Day', x_axis_type = 'datetime',
+               #y_axis_label = '', 
+               y_range = yrng, x_range = xrng)
+    p.line(xrng,[0,0], color = 'black')
+    
+    p.line([mtg_date,mtg_date], [0,100], color ='black')
+    p.line(xdata, 1, color ='black', line_dash = 'dashed')
+    colors = list(plasma(len(tks))[::-1])
+    for t in tks:
+        p.line(xdata,df[t], color = colors.pop(), legend_label = '${}'.format(t))
+    
+    p.xaxis[0].ticker.desired_num_ticks = 10
+    p.legend.location = 'bottom_right'
+    p.ygrid.grid_line_color = None
+    #p.yaxis.formatter=NumeralTickFormatter(format="0.0%")
+
+    export_png(p, filename='imgs/chart1.png')
+
+    return p
+
+# Chart of a regression e.g. inflation vs money supply
+def chart2(df):
+    xdata, ydata, xrng, yrng = set_up(df['_'], df['_'], 
+                                      truncated = False, margins = .005)
+    
+    p = figure(width = 500, height = 500,
+               title="_" , 
+               x_axis_label = '_', 
+               y_axis_label = '_', 
+               y_range = yrng, x_range = xrng)
+    p.line(xrng,[0,0], color = 'black')
+    p.line([0,0],yrng, color = 'black')
+    
+    slope, intercept, r_value, p_value, std_err = stats.linregress(xdata, ydata)
+    leg = 'R = {:.4f}, P-Value = {:.4e}, Slope = {:.4f}'.format(r_value,p_value,slope)
+    p.line(xdata, xdata*slope + intercept, legend = leg, color = 'black')
+    p.circle(xdata,ydata, color = 'blue',size = 2)
+    
+    p.xaxis[0].ticker.desired_num_ticks = 10
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = None
+    p.xaxis.formatter=NumeralTickFormatter(format="0.0%")
+    p.yaxis.formatter=NumeralTickFormatter(format="0.0%")
+    
+    export_png(p, filename='images/chart2.png')
+    
+    return p
+
+show(chart1(df))
